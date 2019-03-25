3f48e56c202ad1b02f05a454d7475fc8b97ac2b1
B533P-15\533 <pluc113@hotmail.com>
2019-03-25 08:13:54 -0400

    debut

---
+++ app/src/main/java/com/example/a533/android_cours_4/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a533.android_cours_4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
